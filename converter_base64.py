fa2aa4a4a55b49c35aab65f2f91346942d453b46
MaMarianaOliveira <mari.unicap27@gmail.com>
2025-07-14 11:50:20 -0300

    loop app work

--- converter_base64.py
+++ converter_base64.py
@@ -17,21 +17,25 @@ def hex_to_base64(hex_string):
         return f"Error converting Hex to Base64: {e}"
     
 def main():
-    print("Choose: ")
-    print("1. Base64 to Hex")
-    print("2. Hex to Base64")
-    choice = input("Enter your choice (1 or 2): ").strip()
+    while True:
+        try:
+            print("Choose: ")
+            print("1. Base64 to Hex")
+            print("2. Hex to Base64")
+            choice = input("Enter your choice (1 or 2): ").strip()
+        except ValueError:
+            print('Invalid Option \n')
 
-    if choice == '1':
-        base64_string = input("Enter Base64 string: ").strip()
-        hex_result = base64_tohex(base64_string)
-        print(f"Hex result: {hex_result}")
-    elif choice == '2':
-        hex_string = input("Enter Hex string: ").strip()
-        base64_result = hex_to_base64(hex_string)
-        print(f"Base64 result: {base64_result}")
-    else:
-        print("Invalid choice. Please enter 1 or 2.")      
+        if choice == '1':
+            base64_string = input("Enter Base64 string: ").strip()
+            hex_result = base64_tohex(base64_string)
+            print(f"Hex result: {hex_result}")
+        elif choice == '2':
+            hex_string = input("Enter Hex string: ").strip()
+            base64_result = hex_to_base64(hex_string)
+            print(f"Base64 result: {base64_result}")
+        else:
+            print("Invalid choice. Please enter 1 or 2.")      
 
 if __name__ == "__main__":
     main()
