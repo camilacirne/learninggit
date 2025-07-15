d7b2731d97d0ab5617f7cb503b830c65b8a8fd46
camilacirne <camilacirne@github.com>
2025-07-15 09:45:18 -0300

    Padding base64 input

--- converter_base64.py
+++ converter_base64.py
@@ -1,8 +1,10 @@
 import base64
 
+
 def base64_tohex(base64_string):
     try:
-        decoded_bytes = base64.b64decode(base64_string)
+        padded_base64_string = base64_padding(base64_string)
+        decoded_bytes = base64.b64decode(padded_base64_string)
         return decoded_bytes.hex()
     except Exception as e:
         return f"Error decoding Base64: {e}"
@@ -15,7 +17,16 @@ def hex_to_base64(hex_string):
         return base64.b64encode(bytes_data).decode('utf-8')
     except Exception as e:
         return f"Error converting Hex to Base64: {e}"
-    
+
+def base64_padding(base64_string):
+    try:
+        padding = ((4 - len(base64_string) % 4))
+        padded_string = base64_string + ("=" * padding)
+        print(f"{padded_string}")
+        return padded_string
+    except Exception as e:
+        return f"Error padding base64 string: {e}" 
+       
 def main():
     print("Choose: ")
     print("1. Base64 to Hex")
@@ -28,7 +39,6 @@ def main():
         print(f"Hex result: {hex_result}")
     elif choice == '2':
         hex_string = input("Enter Hex string: ").strip()
-        #hex_string = hex_size(hex_string)
         base64_result = hex_to_base64(hex_string)
         print(f"Base64 result: {base64_result}")
     else:
