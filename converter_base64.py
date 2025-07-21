5537b05e9d7deb402d142e04a732805b457a9898
camilacirne <camilacirne@github.com>
2025-07-21 09:53:51 -0300

    Organize file

--- converter_base64.py
+++ converter_base64.py
@@ -1,5 +1,5 @@
+import sys
 import base64
-
 
 def base64_tohex(base64_string):
     try:
@@ -20,29 +20,9 @@ def hex_to_base64(hex_string):
 
 def base64_padding(base64_string):
     try:
-        padding = ((4 - len(base64_string) % 4))
+        padding = ((4 - len(base64_string) % 4) % 4)
         padded_string = base64_string + ("=" * padding)
-        print(f"{padded_string}")
         return padded_string
     except Exception as e:
-        return f"Error padding base64 string: {e}" 
+        return f"Error padding Base64 string: {e}" 
        
-def main():
-    print("Choose: ")
-    print("1. Base64 to Hex")
-    print("2. Hex to Base64")
-    choice = input("Enter your choice: ").strip()
-
-    if choice == '1':
-        base64_string = input("Enter Base64 string: ").strip()
-        hex_result = base64_tohex(base64_string)
-        print(f"Hex result: {hex_result}")
-    elif choice == '2':
-        hex_string = input("Enter Hex string: ").strip()
-        base64_result = hex_to_base64(hex_string)
-        print(f"Base64 result: {base64_result}")
-    else:
-        print("Invalid choice. Please enter 1 or 2.")      
-
-if __name__ == "__main__":
-    main()
---
+++ original_main.py
@@ -0,0 +1,2 @@
+from converter_base64 import base64_tohex, hex_to_base64
+
